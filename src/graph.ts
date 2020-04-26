639f8b05527594b409504e6f6a9e12a8dbad7dc7
willypapa <william.cheung.c@gmail.com>
2020-04-26 13:13:56 +0100

    added more graph definitions

--- src/graph.ts
+++ src/graph.ts
@@ -10,11 +10,23 @@ import { IncidenceMap } from "./incidence-map";
 export class Graph {
 
     constructor(
-        public vertices: Vertex[] = [],
-        public edges: Edge[] = [],
-        public incidenceMap: IncidenceMap
+        private _vertices: Vertex[] = [],
+        private _edges: Edge[] = [],
+        private _incidenceMap: IncidenceMap
     ) {
         this.checkIncidenceMap();
+    }
+
+    vertices(): Vertex[] {
+        return this._vertices;
+    }
+
+    edges(): Edge[] {
+        return this._edges;
+    }
+
+    incidenceMap(): IncidenceMap {
+        return this._incidenceMap;
     }
 
     /**
@@ -24,16 +36,16 @@ export class Graph {
      * @param first
      * @param second 
      */
-    adjacentVertices(
+    adjacent(
         first: Vertex,
         second: Vertex
     ): boolean {
-        if (this.edges === []) {
+        if (this._edges === []) {
             return false;
         }
 
-        for (const edge of this.edges) {
-            const [end1, end2] = this.incidenceMap(edge);
+        for (const edge of this._edges) {
+            const [end1, end2] = this._incidenceMap(edge);
 
             const isConnectingEdge: boolean =
                 (first.equalTo(end1) && second.equalTo(end2))
@@ -62,13 +74,13 @@ export class Graph {
             return false;
         }
 
-        return this.adjacentVertices(first, second);
+        return this.adjacent(first, second);
     }
 
     allNeighbours(check: Vertex): Set<Vertex> {
         const neighbours: Set<Vertex> = new Set();
 
-        for (const vertex of this.vertices) {
+        for (const vertex of this._vertices) {
             if (this.neighbours(check, vertex)) {
                 neighbours.add(vertex);
             }
@@ -107,8 +119,8 @@ export class Graph {
     }
 
     hasLoops(): boolean {
-        for (const edge of this.edges) {
-            if (edge.loop(this.incidenceMap)) {
+        for (const edge of this._edges) {
+            if (edge.loop(this._incidenceMap)) {
                 return true;
             }
         }
@@ -116,22 +128,130 @@ export class Graph {
         return false;
     }
 
-
     hasParallelEdges(): boolean {
         // TODO: Can we make this more efficient?
-        const edgesToCheck = this.edges;
+        const edgesToCheck = this._edges.splice(0);
 
-        for (const edge1 of this.edges) {
+        for (const edge1 of this._edges) {
             edgesToCheck.shift();
 
             for (const edge2 of edgesToCheck) {
-                if (edge1.parallel(edge2, this.incidenceMap)) {
+                if (edge1.parallel(edge2, this._incidenceMap)) {
                     return true;
                 }
             }
         }
 
         return false;
+    }
+
+    /**
+     * A graph is a complete graph if it is 
+     * a simple graph and where any pair of 
+     * vertices are adjacent.
+     */
+    complete(): boolean {
+        // TODO
+        return true;
+    }
+
+    /**
+     * A graph is an empty graph if no two 
+     * vertices are adjacent (i.e. there 
+     * are no edges).
+     */
+    empty(): boolean {
+        return this.edges.length === 0;
+    }
+
+    /**
+     * A graph is bipartite if its vertices can 
+     * be partitioned into two such that every 
+     * edge has an end in each. The two sets are
+     * called a bipartition of the graph.
+     */
+    bipartite(): boolean {
+        // TODO
+        return false;
+    }
+
+    completeBipartite(): boolean {
+        return this.complete()
+            && this.bipartite();
+    }
+
+    /**
+     * A star graph is a complete bipartite graph
+     * where the size of one of the bipartitions
+     * is equal to 1.
+     */
+    star(): boolean {
+        /**
+         * TODO
+         *  Find bipartition
+         *  Check one of them has size = 1
+         */
+        return false;
+    }
+
+    /**
+     * A path is a simple graph whose vertices can
+     * be arranged in a linear sequence such that
+     * consecutive vertices are adjacent.
+     */
+    path(): boolean {
+        // TODO
+        return false;
+    }
+
+    /**
+     * The length of a path is its number of edges.
+     * A path of length k is called a k-path.
+     * 
+     * Returns null if graph is not a path.
+     */
+    pathLength(): number | null {
+        return this.path()
+            ? this.vertices.length
+            : null;
+    }
+
+    /**
+     * A cycle is a graph with 3 or more verrtices
+     * arranged in a cyclic sequence such that 
+     * consecutive vertices are adjacent.
+     */
+    cycle(): boolean {
+        // TODO
+        return false;
+    }
+
+    /**
+     * The length of a cycle is its number of edges.
+     * A cycle of length k is called a k-cycle.
+     * 
+     * Returns null if graph is not a cycle.
+     */
+    cycleLength(): number | null {
+        return this.cycle()
+            ? this.vertices.length
+            : null;
+    }
+
+    triangle(): boolean {
+        return this.cycleLength() === 3;
+    }
+
+    quadilateral(): boolean {
+        return this.cycleLength() === 4;
+    }
+
+    pentagon(): boolean {
+        return this.cycleLength() === 5;
+    }
+
+    hexagon(): boolean {
+        return this.cycleLength() === 6;
     }
 
     private checkIncidenceMap(): void {
